194c01da1996d6c767e225577d89f363856164ea
Joshua Samuel Cantor <jcantor@joshuas-air.gateway.sonic.net>
2016-06-29 19:23:02 -0700

    serves index.html

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express'),
 	app = express();
 
+app.get("/", function(req, res, next) {
+	res.sendFile(process.cwd() + "/server/index.html");
+});
 
 
 app.listen(3000, function() {
